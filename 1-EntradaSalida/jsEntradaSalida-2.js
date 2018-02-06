6a1d9bea7e38e5b5ac1efe48e494d111d6625479
EzequielSegota <36193787+EzequielSegota@users.noreply.github.com>
2018-02-06 10:16:00 -0300

    Ejercicios 1 y 2
    Prompt, Var, Concatenar, Comentar, Alert

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,18 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+	var nombre;
+	//esto es yun comentario de una linea
+
+	//alert(nombre)
+	/*
+	esto es un bloque
+	alert("nombre")
+	nombre="Ezequiel"
+	alert(nombre);
+	Recordar prompt lleva detras =dato
+	*/
+	nombre=prompt("Ingrese nombre", "Natalia Natalia");
+	alert("Su nombre es: "+nombre);
 }
 
