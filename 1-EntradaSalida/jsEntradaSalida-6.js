60a58ee74331b1a706e4ac6e49fcb88af4d31a48
EzequielSegota <36193787+EzequielSegota@users.noreply.github.com>
2018-02-06 17:04:07 -0300

    ParseInt

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -5,10 +5,12 @@ function sumar()
 {
 	var Numero1;
 	var Numero2;
+	var sumar
 
 	Numero1=document.getElementById('numeroUno').value;
 	Numero2=document.getElementById('numeroDos').value;
+	sumar=(parseInt(Numero1) + parseInt(Numero2));
 
-	
+	alert("La suma de los numeros es: "+sumar );
 }
 
