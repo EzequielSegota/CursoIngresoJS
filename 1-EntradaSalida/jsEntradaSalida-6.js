2cb89d75af07ab078fc92bae13a15793c180ab03
EzequielSegota <36193787+EzequielSegota@users.noreply.github.com>
2018-02-07 10:59:27 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,14 +3,17 @@ transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	var Numero1;
-	var Numero2;
-	var sumar
+	var numeroUno;
+	var numeroDos;
+	var sumar;
 
-	Numero1=document.getElementById('numeroUno').value;
-	Numero2=document.getElementById('numeroDos').value;
-	sumar=(parseInt(Numero1) + parseInt(Numero2));
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroDos=document.getElementById('numeroDos').value;
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
 
-	alert("La suma de los numeros es: "+sumar );
+	sumar=numeroUno+numeroDos
+
+	alert("La suma de los numeros es: "+sumar);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,64 @@ mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var numeroUno;
+	var numeroDos;
+	var sumar;
+
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroDos=document.getElementById('numeroDos').value;
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+
+	sumar=numeroUno+numeroDos;
+
+	alert("La suma es: "+sumar);
 }
 
 function restar()
 {
-	
+	var numeroUno;
+	var numeroDos;
+	var resta;
+
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroDos=document.getElementById('numeroDos').value;
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+
+	resta=numeroUno-numeroDos;
+
+	alert("La resta es: "+resta);
 }
 
 function multiplicar()
 { 
-	
+	var numeroUno;
+	var numeroDos;
+	var multiplicar;
+
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroDos=document.getElementById('numeroDos').value;
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+
+	multiplicar=numeroUno*numeroDos;
+
+	alert("La multiplicación es: "+multiplicar);
 }
 
 function dividir()
 {
-	
-}
+	var numeroUno;
+	var numeroDos;
+	var dividir;
 
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroDos=document.getElementById('numeroDos').value;
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+
+	dividir=numeroUno/numeroDos;
+
+	alert("La división es: "+dividir);
+}
