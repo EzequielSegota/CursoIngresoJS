968c03c2f291d7a3d0b9456b7aa2a1725c453d41
EzequielSegota <36193787+EzequielSegota@users.noreply.github.com>
2018-02-07 21:09:33 -0300

    Terminado

--- 9-Parciales/parcial2/2-entradaSalida.js
+++ 9-Parciales/parcial2/2-entradaSalida.js
@@ -1,7 +1,13 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 2-EntradaSalida");
-	
+	var importe;
+	var IVA;
+
+	importe=prompt("Ingrese importe");
+
+	IVA=importe*1.21;
+
+	document.getElementById('importe').value=IVA;
 }
 
