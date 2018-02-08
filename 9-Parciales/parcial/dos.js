0690ed7ce6eb5d11a1c45db292352c1e6a6ce514
EzequielSegota <36193787+EzequielSegota@users.noreply.github.com>
2018-02-07 21:09:05 -0300

    Arreglados

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,4 +1,9 @@
 function Mostrar()
 {
-  
+  var importe;
+  var importeFinal;
+
+  importe=prompt("Ingrese importe");
+  importeFinal=importe*1.21
+  document.getElementById('importeFinal').value=importeFinal;
 }
--- 9-Parciales/parcial/tres.js
+++ 9-Parciales/parcial/tres.js
@@ -1,4 +1,16 @@
 function Mostrar()
 {
+	var largo;
+	var ancho;
+	var perimetro;
+	var resultado;
 
+	largo=document.getElementById('largo').value;
+	ancho=document.getElementById('ancho').value;
+	largo=parseInt(largo);
+	ancho=parseInt(ancho);
+	perimetro=largo+largo+ancho+ancho
+	resultado=perimetro*3
+
+	alert("Los metros de alambre que se necesitan son: "+resultado);
 }
