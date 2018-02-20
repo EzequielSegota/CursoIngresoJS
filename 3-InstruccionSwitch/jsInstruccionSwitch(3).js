52cd0371c368a3a62de17a7390dd393ea6175c63
EzequielSegota <36193787+EzequielSegota@users.noreply.github.com>
2018-02-20 10:59:15 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,10 +1,21 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
-	
+	switch(mesDelAño)
+	{
+		case "Febrero":
+			if(mesDelAño=="Febrero")
+			{
+				alert("Este mes tiene 29 dìas");
+			} 
+			else
+			{
+				alert("Este mes tiene 30 o màs dìas");
+			}
+	}
+
 	
 
 
