8555aa7b209bc5b2ad6788a33d972bd210ce5ddc
EzequielSegota <36193787+EzequielSegota@users.noreply.github.com>
2018-02-21 11:13:05 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,46 @@
 function Mostrar()
 {
+	var contador=0
+	var iteraciones=10
+	var contadorDePares=0;
+	var contadorDeImpares=0;
+	var contadorDeCeros=0;
+	var contadorDePositivos=0;
+	var contadorDeNegativos=0;
 
+	for(contador=-10;contador<iteraciones;contador++)
+	{
+		document.write("<br>"+contador);
+		if (contador==0)
+		{
+			contadorDeCeros++;
+		}
+		else
+		{
+			if(contador<0)
+			{
+				contadorDeNegativos++;
+			}
+			if(contador>0)
+			{
+				contadorDePositivos++;
+			}
+			if(contador%2==0)
+			{
+				contadorDePares++;
+			}
+			else
+			{
+			contadorDeImpares++;
+			}
+		}
+		
+		
+	}
+		//document.write("<br>"+contador);		
+		document.write("<br> cantidad de pares: "+contadorDePares);
+		document.write("<br> cantidad de impares: "+contadorDeImpares);
+		document.write("<br> cantidad de ceros: "+contadorDeCeros);
+		document.write("<br> cantidad de positivos: "+contadorDePositivos);
+		document.write("<br> cantidad de negativos: "+contadorDeNegativos);
 }
