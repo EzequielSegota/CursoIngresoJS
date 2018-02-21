c106144ccacd542ae91238958840b7754413737e
EzequielSegota <36193787+EzequielSegota@users.noreply.github.com>
2018-02-21 11:13:31 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,10 +3,18 @@ function Mostrar()
 
 	var contador=0;
 	var acumulador=0;
+	var numero
+
+	while(contador<5)
+	{
+		contador++;
+		numero=prompt("Ingrese numero");
+		numero=parseInt(numero);
+		acumulador=acumulador+numero;
+	}
 
 
-
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+	document.getElementById('suma').value=acumulador;
+	document.getElementById('promedio').value=acumulador/5;
 
 }//FIN DE LA FUNCIÓN
