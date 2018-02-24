9ac6def744fda0bf393a329bb2bd99a0b09caabc
EzequielSegota <36193787+EzequielSegota@users.noreply.github.com>
2018-02-24 12:10:39 -0300

    Add files via upload

--- 9-Parciales/parcial/ocho.js
+++ 9-Parciales/parcial/ocho.js
@@ -1,4 +1,120 @@
+/*Ingreso de alumnos
+No sabemos cuantos alumnos hay
+pedimos el nombre
+¿cuantos alumnos ingrese?
+document.write se ingresaron x alumnos
+*/
 function Mostrar()
 {
+	var ingresarAlumnos=""
+	var alumnos;
+	var cantidadDeAlumnos=0;
+	var nota;
+	var acumuladorDeNota=0;
+	var promedio;
+	var sexo;
+	var contadorMasculino=0;
+	var contadorFemenino=0;
+	var hombresDesaprobados=0;
+	var mejorNota=0;
+	var nombreMaxNota;
+	var notaMujeres=0;
+	var promedioNotaMujeres;
+	var menorEdad=0;
+	var edad=0;
+	var sexoMenorDeEdad;
+	var edadesAprobados=0;
+	var promedioEdadDeAprobados;
+	var cantidadDeAlumnosAprobados=0;
+	var notasPares=0;
+	var notasImpares=0;
 
+
+	while(ingresarAlumnos != "no")
+	{
+		alumnos=prompt("Ingrese nombre: ");
+		cantidadDeAlumnos++;
+		
+		nota=prompt("Ingrese nota: ");
+		nota=parseInt(nota);
+		while(nota<0 || nota>10)
+		{
+			nota=prompt("Ingrese nota: ");
+			nota=parseInt(nota);
+		}
+		
+		sexo=prompt("Ingrese sexo F ó M");
+
+		while(sexo !="F" && sexo != "M")
+		{
+			sexo=prompt("Ingrese sexo F ó M");
+		}
+		if(sexo=="F")
+		{
+			contadorFemenino++;
+			notaMujeres=notaMujeres+nota;
+		}
+		else
+		{
+			contadorMasculino++;
+		}
+		
+		edad=prompt("Ingrese edad");
+
+		while(edad<0 || edad>100)
+		{
+			edad=prompt("Ingrese edad");
+		}
+
+
+		if(sexo=="M" && nota<4)
+		{
+			hombresDesaprobados++;
+		}
+		
+		if(nota>mejorNota)
+		{
+			mejorNota=nota;
+			nombreMaxNota=alumnos;
+		}
+		
+		if(menorEdad>edad)
+		{
+			menorEdad=edad;
+			sexoMenorDeEdad=sexo
+		}
+
+		if(nota>3)
+		{
+			cantidadDeAlumnosAprobados++;
+			edadesAprobados=edadesAprobados+edad;
+		}
+
+		if(nota%2==0)
+		{
+			notasPares++;
+		}
+		else
+		{
+			notasImpares++;
+		}
+
+		acumuladorDeNota=acumuladorDeNota+nota;
+
+		ingresarAlumnos=prompt("ingrese no para salir");
+	}
+	document.write("<br>Son "+cantidadDeAlumnos+" alumnos");
+	promedio=acumuladorDeNota/cantidadDeAlumnos;
+	document.write("<br>El promedio es: "+promedio);
+	document.write("<br>La cantidad de mujeres: "+contadorFemenino);
+	document.write("<br>La cantidad de hombres es: "+contadorMasculino);
+	document.write("<br>Los cantidad de hombres desaprobados es: "+hombresDesaprobados);
+	document.write("<br>La mejor nota es de: "+nombreMaxNota);
+	promedioNotaMujeres=notaMujeres/contadorFemenino;
+	document.write("<br>El promedio de notas femeninas es: "+promedioNotaMujeres);
+	document.write("<br>El sexo del menor de edad es: "+sexoMenorDeEdad);
+	promedioEdadDeAprobados=edadesAprobados/cantidadDeAlumnosAprobados;
+	document.write("<br>El promedio de las edades de los alumnos aprobados es: "+promedioEdadDeAprobados);
+	document.write("<br>Cantidad de notas pares es: "+notasPares);
+	document.write("<br>Cantidad de notas impares es: "+notasImpares);
 }
--- 9-Parciales/parcial/seis.js
+++ 9-Parciales/parcial/seis.js
@@ -1,4 +1,33 @@
 function Mostrar()
 {
+	var importe;
+	var contador=0;
+	var iteraciones=24;
+	var importeMaximo=0;
+	var importeMinimo=0;
+	
 
+	while(contador<iteraciones)
+	{
+		contador++;
+		importe=prompt("Ingrese importe");
+		
+		if(importe<=0)
+		{
+			alert("Numero no valido");
+			continue;
+		}
+		else
+		{
+			if(importe>importeMaximo)
+				{
+					importeMaximo=importe;
+				}
+			else(importe<importeMinimo)
+				{
+					importeMinimo=importe;
+				}
+		}
+	}
+	importe=document.getElementById('importeFinal');
 }
