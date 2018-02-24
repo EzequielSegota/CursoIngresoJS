b98cee397f194ee66c62d599215e2e87b0269c29
EzequielSegota <36193787+EzequielSegota@users.noreply.github.com>
2018-02-24 17:18:07 -0300

    Add files via upload

--- 9-Parciales/parcial2/6-iteraciones.js
+++ 9-Parciales/parcial2/6-iteraciones.js
@@ -1,7 +1,33 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 6-iteraciones");
-	
+	var importeDeVentas;
+	var iteraciones=7;
+	var contador=0
+	var importeMax=0;
+	var importeMin=99999999999;
+
+
+	while(contador<iteraciones)
+	{
+		contador++;
+		importeDeVentas=prompt("Ingrese el importe");
+		importeDeVentas=parseInt(importeDeVentas);
+		while(importeDeVentas==0)
+		{
+			importeDeVentas=prompt("Ingrese el importe");
+			importeDeVentas=parseInt(importeDeVentas);
+		}
+		if(importeDeVentas<importeMin)
+		{
+			importeMin=importeDeVentas;
+		}
+		if(importeDeVentas>importeMax)
+		{
+			importeMax=importeDeVentas;
+		}
+	}
+	document.write("<br>El importe Maximo es: "+importeMax);
+	document.write("<br >El importe Minimo es: "+importeMin);
 }
 
