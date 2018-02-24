510668109d3959d494f72afadf457311480fbfde
EzequielSegota <36193787+EzequielSegota@users.noreply.github.com>
2018-02-24 17:17:45 -0300

    Add files via upload

--- 9-Parciales/parcial/cinco.js
+++ 9-Parciales/parcial/cinco.js
@@ -1,4 +1,27 @@
 function Mostrar()
 {
+	var semana;
 
+	semana=prompt("Ingrese día");
+
+	switch(semana)
+	{
+		case "Lunes":
+
+		case "Martes":
+
+		case "Miercoles":
+
+		case "Jueves":
+
+		case "Viernes":
+			alert("“a trabajar !!!” .");
+			break;
+
+		case "Sábado":
+			
+		case "Domingo":
+			alert("“es fin de semana”");
+			break;
+	}
 }
--- 9-Parciales/parcial/cuatro.js
+++ 9-Parciales/parcial/cuatro.js
@@ -1,4 +1,32 @@
 function Mostrar()
 {
+	var numeroUno;
+	var numeroDos;
+	var multiplicacion;
+	var suma;
+	var resta;
 
+	numeroUno=prompt("Ingrese 1er número: ");
+	numeroDos=prompt("Ingrese 2do número: ");
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+
+	if(numeroUno==numeroDos)
+	{
+		multiplicacion=numeroUno*numeroDos
+		document.write("El resultado de la multiplicacion es: "+multiplicacion);
+	}
+	else
+	{
+		if(numeroUno>numeroDos)
+		{
+			resta=numeroUno-numeroDos;
+			document.write("El resultado de la resta es: "+resta);
+		}
+		else
+		{
+			suma=numeroUno+numeroDos;
+			document.write("El resultado de la suma es: "+suma);
+		}
+	}	
 }
