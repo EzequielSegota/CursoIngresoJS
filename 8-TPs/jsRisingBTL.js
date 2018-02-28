de69431573537482e1852071a1c2d0a6d675d43e
EzequielSegota <36193787+EzequielSegota@users.noreply.github.com>
2018-02-28 11:54:05 -0300

    Add files via upload

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -9,5 +9,71 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
- 
+ 	var edad;
+ 	var sexo;
+ 	var estadoCivil;
+ 	var sueldoBruto;
+ 	var numLegajo;
+ 	var nacionalidad;
+ 	var contadorLegajo=0;
+ 	var legajo;
+
+
+ 		edad=prompt("Ingrese edad");
+
+ 		while(edad<18 || edad>90 || isNaN(edad) || edad=="")
+ 		{
+ 			edad=prompt("Ingrese edad");
+ 		}
+
+ 		sexo=prompt("Ingrese sexo, F ó M:");
+ 		
+ 		while(sexo!="F" && sexo!="M")
+ 		{
+ 			sexo=prompt("Ingrese sexo, F ó M:");
+ 		}
+
+ 		estadoCivil=prompt("Ingrese estado civil, 1-para soltero, 2-para casados, 3-para divorciados y 4-para viudos")
+
+ 		while(estadoCivil<1 || estadoCivil>4 && isNaN(estadoCivil) && estadoCivil=="")
+ 		{
+ 			 	estadoCivil=prompt("Ingrese estado civil, 1-para soltero, 2-para casados, 3-para divorciados y 4-para viudos")
+ 		}
+
+ 		sueldoBruto=prompt("Ingrese sueldo bruto");
+
+ 		while(sueldoBruto<8000 || isNaN(sueldoBruto) || estadoCivil=="")
+ 		{
+ 			sueldoBruto=prompt("Ingrese sueldo bruto no menor a 8000");
+ 		}
+
+ 		numLegajo=prompt("Ingrese numero legajo:");
+ 		numLegajo=parseInt(numLegajo);
+
+ 		while(legajo>=1)
+ 		{
+ 			contadorLegajo++;
+ 			legajo=numLegajo/10;
+ 			numLegajo=parseInt(numLegajo);
+ 		}
+
+ 		while(numLegajo<1000 || numLegajo>9999 || contadorLegajo!=4 || isNaN(numLegajo) || numLegajo=="")
+ 		{
+ 			numLegajo=prompt("Ingrese numero legajo:");
+ 			numLegajo=parseInt(numLegajo);
+ 		}
+
+ 		nacionalidad=prompt("Ingrese nacionalidad: “A” para argentinos, “E” para extranjeros, “N” para nacionalizados.");
+
+ 		while(nacionalidad!=isNaN(nacionalidad) && nacionalidad!="A" && nacionalidad!="E" && nacionalidad!="N")
+ 		{
+ 			nacionalidad=prompt("Ingrese nacionalidad: “A” para argentinos, “E” para extranjeros, “N” para nacionalizados.");
+ 		}
+
+ 	document.getElementById('Edad').value=edad;
+ 	document.getElementById('Sexo').value=sexo;
+ 	document.getElementById('EstadoCivil').value=estadoCivil;
+ 	document.getElementById('Sueldo').value=sueldoBruto;
+ 	document.getElementById('Legajo').value=numLegajo;
+ 	document.getElementById('Nacionalidad').value=nacionalidad;
 }
